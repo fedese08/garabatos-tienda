38add188f20f7db61924140bc1c3da2cd6f5b887
fedese08 <fedesena2000@gmail.com>
2022-01-31 19:26:08 -0300

    Actualizacion 31-01

---
+++ src/apis/api.js
@@ -0,0 +1,28 @@
+const products = [
+    { 
+        id: 1,
+        title: 'Zapatilla Atomik',
+        desctiption: 'Zapatilla de Atomik blanca con abrojo',
+        price: 2500,
+        imageURL: "https://atomik.com.ar/wp-content/uploads/2021/02/DSCF3757.jpg",    
+    },
+    { 
+        id: 2,
+        title: 'Zapato Marcel',
+        desctiption: 'Zapato de Marcel escolar marron',
+        price: 3900,
+        imageURL: "https://marcelnet.com.ar/wp-content/uploads/2021/09/500N-MARRON-DIA.jpg",
+    },
+    { 
+        id: 3,
+        title: 'Croc Amarilla',
+        desctiption: 'Croc amarilla ',
+        price: 3200,
+        imageURL: "https://crocsarg.vtexassets.com/arquivos/ids/159552-800-auto?width=800&height=auto&aspect=true",
+    }
+];
+
+
+export {
+    products,
+}
